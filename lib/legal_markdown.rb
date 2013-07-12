4bdc624c831b08e8055ea791dc69de81c7de8755
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2013-07-12 03:22:22 +0300

    Fix spaces before newlines

--- lib/legal_markdown.rb
+++ lib/legal_markdown.rb
@@ -418,7 +418,7 @@ class LegalToMarkdown
   # ----------------------
   # Write the file
   def write_it( final_content )
-    final_content = final_content.squeeze(" ")
+    final_content = final_content.squeeze(" ").gsub(/ +\n/, "\n")
     if @output_file && @output_file != "-"
       File.open(@output_file, "w") {|f| f.write( final_content ) }
     else
