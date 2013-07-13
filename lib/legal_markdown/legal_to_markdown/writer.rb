04968a06330537a223dfb878b8d5a3a86659296d
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2013-07-13 20:01:38 +0300

    Finally fix that damn testing bug...flose the file after writing it fool!

--- lib/legal_markdown/legal_to_markdown/writer.rb
+++ lib/legal_markdown/legal_to_markdown/writer.rb
@@ -4,7 +4,7 @@ module LegalToMarkdown
   def write_it( final_content )
     final_content = final_content.gsub(/ +\n/, "\n")
     if @output_file && @output_file != "-"
-      File.open(@output_file, "w") {|f| f.write( final_content ) }
+      File.open(@output_file, "w") {|f| f.write( final_content ); f.close }
     else
       STDOUT.write final_content
     end
