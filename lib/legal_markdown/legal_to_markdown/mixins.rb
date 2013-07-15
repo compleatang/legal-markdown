5f96dd1990efeab2d4390bfafa01f893995079e5
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2013-07-15 18:07:35 +0300

    Refactor mixin module a bit

--- lib/legal_markdown/legal_to_markdown/mixins.rb
+++ lib/legal_markdown/legal_to_markdown/mixins.rb
@@ -11,42 +11,14 @@ module LegalToMarkdown
     end
 
     def clauses_mixins
-      clauses_to_delete = []
-      clauses_to_mixin = []
+      @clauses_to_delete = []
+      @clauses_to_mixin = []
+      set_up_opt_clauses
+      backup_opt_clauses
 
-      @headers.each do | mixin, replacer |
-        replacer = replacer.to_s.downcase
-        clauses_to_delete << mixin if replacer == "false"
-        clauses_to_mixin << mixin if replacer == "true"
-      end
-
-      clauses_to_delete.each { |m| @headers.delete(m) }
-      clauses_to_mixin.each { |m| @headers.delete(m) }
-      clauses_deleted = clauses_to_delete.dup
-      clauses_added = clauses_to_mixin.dup
-
-      until clauses_added.size == 0 && clauses_deleted.size == 0
-        clauses_added.each do | mixin |
-          pattern = /(\[\{\{#{mixin}\}\}\s*?)(.*?\n*?)(\])/m
-          sub_pattern = /\[\{\{(\S+?)\}\}\s*?/
-          @content[pattern]
-          get_it_all = $& || ""
-          sub_clause = $2 || ""
-          next if sub_clause[sub_pattern] && ( clauses_to_mixin.include?($1) || clauses_to_delete.include?($1) )
-          @content = @content.gsub( get_it_all, sub_clause.lstrip )
-          clauses_added.delete( mixin ) unless @content[pattern]
-        end
-
-        clauses_deleted.each do | mixin |
-          pattern = /(\[\{\{#{mixin}\}\}\s*?)(.*?\n*?)(\])/m
-          sub_pattern = /\[\{\{(\S+?)\}\}\s*?/
-          @content[pattern]
-          get_it_all = $& || ""
-          sub_clause = $2 || ""
-          next if sub_clause[sub_pattern] && ( clauses_to_mixin.include?($1) || clauses_to_delete.include?($1) )
-          @content = @content.gsub( get_it_all, "" )
-          clauses_deleted.delete( mixin ) unless @content[pattern]
-        end
+      until @clauses_added.size == 0 && @clauses_deleted.size == 0
+        clauses_churn_add
+        clauses_churn_delete
       end
     end
 
@@ -65,6 +37,47 @@ module LegalToMarkdown
       @content.gsub!(/(\n\n+)/, "\n\n")
       @content.squeeze!(" ")
       @headers = nil if @headers.empty?
+    end
+
+    def set_up_opt_clauses
+      @headers.each do | mixin, replacer |
+        replacer = replacer.to_s.downcase
+        @clauses_to_delete << mixin if replacer == "false"
+        @clauses_to_mixin << mixin if replacer == "true"
+      end
+    end
+
+    def backup_opt_clauses
+      @clauses_to_delete.each { |m| @headers.delete(m) }
+      @clauses_to_mixin.each { |m| @headers.delete(m) }
+      @clauses_deleted = @clauses_to_delete.dup
+      @clauses_added = @clauses_to_mixin.dup
+    end
+
+    def clauses_churn_add
+      @clauses_added.each do | mixin |
+        pattern = /(\[\{\{#{mixin}\}\}\s*?)(.*?\n*?)(\])/m
+        sub_pattern = /\[\{\{(\S+?)\}\}\s*?/
+        @content[pattern]
+        get_it_all = $& || ""
+        sub_clause = $2 || ""
+        next if sub_clause[sub_pattern] && ( @clauses_to_mixin.include?($1) || @clauses_to_delete.include?($1) )
+        @content = @content.gsub( get_it_all, sub_clause.lstrip )
+        @clauses_added.delete( mixin ) unless @content[pattern]
+      end
+    end
+
+    def clauses_churn_delete
+      @clauses_deleted.each do | mixin |
+        pattern = /(\[\{\{#{mixin}\}\}\s*?)(.*?\n*?)(\])/m
+        sub_pattern = /\[\{\{(\S+?)\}\}\s*?/
+        @content[pattern]
+        get_it_all = $& || ""
+        sub_clause = $2 || ""
+        next if sub_clause[sub_pattern] && ( @clauses_to_mixin.include?($1) || @clauses_to_delete.include?($1) )
+        @content = @content.gsub( get_it_all, "" )
+        @clauses_deleted.delete( mixin ) unless @content[pattern]
+      end
     end
   end
 end
