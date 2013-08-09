38cee78296c8da6c819f2b87c09ca209dce21587
Casey Kuhlman <caseykuhlman@watershedlegal.com>
2013-08-09 15:09:52 +0300

    Simplify MakeYAMLFrontmatter class

--- lib/legal_markdown/make_yaml_frontmatter.rb
+++ lib/legal_markdown/make_yaml_frontmatter.rb
@@ -4,39 +4,31 @@ require 'yaml'
 class MakeYamlFrontMatter
 
   def initialize(args)
-    find_yaml_if_yaml(load(args))
+    @input_file = args[-2] ? args[-2] : args[-1]
+    @output_file = args[-1]
+    find_yaml_if_yaml load
     scan_and_filter_yaml
     build_new_yaml_frontmatter unless @yaml_data_as_array == [{},{},{},{}]
     write_it
   end
 
-  def load(args)
+  private
+
+  def load
     begin
-      @input_file = args[-2] ? args[-2] : args[-1]
-      @output_file = args[-1]
-      if @input_file != "-"
-        source_file = File::read(@input_file) if File::exists?(@input_file) && File::readable?(@input_file)
-      else
-        source_file = STDIN.read
-      end
-      source_file.scan(/(@include (.+)$)/).each do |set|
-        partial_file = set[1]
-        to_replace = set[0]
-        partial_contents = File::read(partial_file) if File::exists?(partial_file) && File::readable?(partial_file)
-        source_file.gsub!(to_replace, "[PARTIALSTART]\n" + partial_contents + "\n[PARTIALENDS][#{to_replace}]")
-      end
-      return source_file
-    rescue => e
-      puts "Sorry, I could not read the input file #{@input_file}: #{e.message}."
+      source_file = @input_file == "-" ? STDIN.read : File::read(@input_file)
+      source_file = guard_partials_start source_file
+    rescue
+      puts "Sorry, I could not read the input file #{@input_file}."
       exit 0
     end
   end
 
-  def find_yaml_if_yaml( source )
+  def find_yaml_if_yaml source
     yaml_pattern = /\A(---\s*\n.*?\n?)^(---\s*$\n?)/m
-    parts = source.partition( yaml_pattern )
-    if parts[1] != ""
-      parts[1] = string_guard parts[1]
+    parts = source.partition yaml_pattern
+    if ! parts[1].empty?
+      parts[1] = guard_strings parts[1]
       @headers = YAML.load parts[1]
       @content = parts[2]
     else
@@ -50,9 +42,9 @@ class MakeYamlFrontMatter
     opt_clauses_pattern = /\[{{(\S+)}}/
     @structured_headers_pattern = /(^l+.|^l\d+.)/
     @yaml_data_as_array = []
-    @yaml_data_as_array << ( filter_yaml(scan_doc(mixin_pattern)) || {} )
-    @yaml_data_as_array << ( filter_yaml(scan_doc(opt_clauses_pattern)) || {} )
-    @yaml_data_as_array << ( filter_yaml(scan_doc(@structured_headers_pattern)) || {} )
+    @yaml_data_as_array << ( filter_yaml mixin_pattern || {} )
+    @yaml_data_as_array << ( filter_yaml opt_clauses_pattern || {} )
+    @yaml_data_as_array << ( filter_yaml @structured_headers_pattern || {} )
     @yaml_data_as_array << ( @yaml_data_as_array.last.empty? ? {} : filter_yaml(%w{no-indent no-reset level-style}) )
   end
 
@@ -71,11 +63,7 @@ class MakeYamlFrontMatter
   end
 
   def write_it
-    @content.scan(/(\[PARTIALSTART\].*?\[PARTIALENDS\]\[(.*?)\])/m).each do |set|
-      replacer = set[1]
-      to_replace = set[0]
-      @content.gsub!(to_replace, replacer)
-    end
+    guard_partials_finish
     if @output_file && @output_file != "-"
       File.open(@output_file, "w") {|f| f.write( @content ); f.close }
     else
@@ -83,21 +71,9 @@ class MakeYamlFrontMatter
     end
   end
 
-  def scan_doc(pattern)
-    found = @content.scan(pattern).uniq.sort.flatten
-    if pattern == @structured_headers_pattern
-      found = convert_ll_to_level_two found
-    end
-    found
-  end
-
-  def convert_ll_to_level_two(levels)
-    # receives an array in form ["l.", "ll.", "lll."] returns array in form ["level-1", "level-2"]
-    levels.inject([]){|arr, level| level[/((l+)\.)|(l(\d+)\.*)/]; $2 ? arr << "level-" + $2.length.to_s : arr << "level-" + $&.delete("l")}
-  end
-
-  def filter_yaml(stuff)
+  def filter_yaml pattern
     # @headers will be a hash, stuff is an array, returns a filtered hash
+    stuff = pattern.is_a?(Regexp) ? scan_doc(pattern) : pattern
     if stuff
       stuff_in_yaml = stuff.inject({}) do |hash, elem|
         @headers.has_key?(elem) ? hash.merge({elem => @headers[elem]}) : hash.merge({elem => ""})
@@ -105,17 +81,55 @@ class MakeYamlFrontMatter
     end
   end
 
-  def sink_it(section)
+  def scan_doc pattern
+    found = @content.scan(pattern).uniq.sort.flatten
+    if pattern == @structured_headers_pattern
+      found = convert_ll_to_level_two found
+    end
+    found
+  end
+
+  def convert_ll_to_level_two levels
+    # receives an array in form ["l.", "ll.", "lll."] returns array in form ["level-1", "level-2"]
+    levels.inject([]) do |arr, level|
+      level[/((l+)\.)|(l(\d+)\.*)/]
+      if $2
+        arr << "level-" + $2.length.to_s
+      else
+        arr << "level-" + $&.delete("l")
+      end
+    end
+  end
+
+  def sink_it section
     section.inject("") do |string, head|
       string << head[0] + ": \"" + ( head[1].to_s.gsub("\"", "\\\"") || "" ) + "\"\n"
       string
     end
   end
 
-  def string_guard strings
+  def guard_partials_start source_file
+    source_file.scan(/(@include (.+)$)/).each do |set|
+      partial_file = set[1]
+      to_replace = set[0]
+      partial_contents = File::read(partial_file) if File::exists?(partial_file) && File::readable?(partial_file)
+      source_file.gsub!(to_replace, "[PARTIALSTART]\n" + partial_contents + "\n[PARTIALENDS][#{to_replace}]")
+    end
+    source_file
+  end
+
+  def guard_strings strings
     if strings =~ /(:\s*(\d+\.))$/
       strings = strings.gsub($1, ": \"" + $2 + "\"" )
     end
     strings
+  end
+
+  def guard_partials_finish
+    @content.scan(/(\[PARTIALSTART\].*?\[PARTIALENDS\]\[(.*?)\])/m).each do |set|
+      replacer = set[1]
+      to_replace = set[0]
+      @content.gsub!(to_replace, replacer)
+    end
   end
 end
